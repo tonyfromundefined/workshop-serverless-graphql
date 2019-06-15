62faa5f7b99b5b546107d9e6dc4fa14e67049976
tonyfromundefined <tony@erion.kr>
2019-06-15 15:23:23 +0900

    implements UD Task

--- projects/server/src/schema/task.ts
+++ projects/server/src/schema/task.ts
@@ -1,4 +1,10 @@
-import { extendType, idArg, objectType, stringArg } from 'nexus'
+import {
+  booleanArg,
+  extendType,
+  idArg,
+  objectType,
+  stringArg,
+} from 'nexus'
 import short from 'short-uuid'
 
 interface ITask {
@@ -73,6 +79,56 @@ export const TaskMutations = extendType({
         TASKS.push(task)
 
         return task
+      },
+    })
+
+    t.field('updateTask', {
+      type: 'Task',
+      args: {
+        id: idArg({
+          required: true,
+        }),
+        content: stringArg(),
+        isDone: booleanArg(),
+      },
+      resolve: async (_parent, args) => {
+        const taskIndex = TASKS.findIndex((task) => task.id === args.id)
+
+        if (taskIndex) {
+          if (args.content) {
+            TASKS[taskIndex].content = args.content
+          }
+          if (args.isDone) {
+            TASKS[taskIndex].isDone = args.isDone
+          }
+
+          return TASKS[taskIndex]
+
+        } else {
+          throw new Error(`${args.id}라는 ID를 가진 Task를 찾을 수 없습니다`)
+        }
+      },
+    })
+
+    t.field('deleteTask', {
+      type: 'Task',
+      args: {
+        id: idArg({
+          required: true,
+        }),
+      },
+      resolve: async (_parent, args) => {
+        const taskIndex = TASKS.findIndex((task) => task.id === args.id)
+
+        if (taskIndex) {
+          const task = TASKS[taskIndex]
+          TASKS.splice(taskIndex, 1)
+
+          return task
+
+        } else {
+          throw new Error(`${args.id}라는 ID를 가진 Task를 찾을 수 없습니다`)
+        }
       },
     })
   },
